bc04c9f4017a10942ca065838b0d88e8d7aaea4f
zengyu <abc123456>
2022-04-21 21:58:51 +0800

    提交代码

---
+++ src/main/java/com/poa/server/entity/PoaPermission.java
@@ -0,0 +1,34 @@
+package com.poa.server.entity;
+
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table
+public class PoaPermission {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+
+
+    private String documentId;
+    private String email;
+    private Integer type;
+
+
+
+
+
+
+
+
+}
